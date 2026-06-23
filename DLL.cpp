4b747e5490f6b8f79a9c6487790a3bd020db1ee5
xinling906 <starzerochang@gmail.com>
2026-06-24 00:55:13 +0800

    Add files via upload

---
+++ DLL.cpp
@@ -0,0 +1,21 @@
+﻿// DLL.cpp : 定義 DLL 的匯出函式。
+//
+
+#include "framework.h"
+#include "DLL.h"
+
+
+// 這是匯出變數的範例
+DLL_API int nDLL=0;
+
+// 這是匯出函式的範例。
+DLL_API int fnDLL(void)
+{
+    return 0;
+}
+
+// 這是已匯出的類別建構函式。
+CDLL::CDLL()
+{
+    return;
+}
